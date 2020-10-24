3ce7db73115fa4df9486763329ff10818fae39a6
Nattawut Kotcha <60160282@go.buu.ac.th>
2020-10-24 15:27:46 +0700

    add test GET '/'

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,15 @@
 const assert = require('assert')
+const reqest = require('supertest')
+const app = require('../index')
 
 describe('Validate setting the test', () => {
     it('should be ok', () => {
-        assert.ok(false)
+        assert.ok(true)
+    })
+})
+
+describe('GET "/"', () => {
+    it('respond whit Hello!', (dons) => {
+        require(app).get('/').expect('Hello', done)
     })
 })
